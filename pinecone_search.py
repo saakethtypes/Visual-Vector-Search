2f0ca636c8d9c46e30947c839e36cfa3c021e070
Saaketh Chaganty <47172497+saakethtypes@users.noreply.github.com>
2023-06-22 15:25:58 +0530

    Update pinecone_search.py

--- pinecone_search.py
+++ pinecone_search.py
@@ -31,7 +31,7 @@ def pinecone_search():
         img = Image.open(uploaded_file)
         uploaded_img_path = "uploaded/" + datetime.now().isoformat().replace(":", "_") + uploaded_file.name
         img.save(uploaded_img_path)
-        pc_api_key = config.api_keys["PINECONE_API_KEY"]
+        pc_api_key = st.secrets["PINECONE_API_KEY"]
         pinecone.init(api_key=pc_api_key, environment="northamerica-northeast1-gcp")
         model = torchvision.models.squeezenet1_1(pretrained=True).eval()
         index = pinecone.Index("pinecone-image-search")
